8bcdfa1afa1cdc6325e7b153ecb3927de607957c
alanrich-dev <alan.a.rich@gmail.com>
2021-06-21 14:31:28 +0200

    added state components and some logic to create and edit notes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [note, setNote] = useState();
+  const [notes, setNotes] = useState([]);
+  const [isBeingEdited, setIsBeingEdited] = useState(null);
+  const [contentEdited, setContentEdited] = useState('');
+  const [search, setSearch] = useState('');
+
+  function createNote(){
+    let newNote = {
+      title:,
+      text:,
+      id: new Date.getTime()
+    };
+  }
+
+  function editNote(id){
+    let editMe = [...notes].filter((notes) => {
+      note.id === id;
+    });
+    editMe.text =
+  }
+
+  function deleteNote(id){
+
+  }
+
+  function searchNotes(id){
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <header></header>
+      {/*the text of the note transforms to an input when edit button clicked*/}
+      { note.id !==== underGoingEditing ?  ( <p>{note.text}</p> ) :
+      ( <div>
+          <input type="text"
+                 onChange={}
+                 value={contentEdited}/>
+          <button onClick={}>Submit</button>
+        </div> )  }
+
+      <button onClick={setIsBeingEdited(note.id)}>Edit</button>
+      <button onClick={}>Delete</button>
+    </div>
     </div>
   );
 }
